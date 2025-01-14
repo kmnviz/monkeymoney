5f72293804f1bc83a07ee026af3a32dbc56ed602
kmnviz <kamenovivanzdravkov@gmail.com>
2025-01-14 18:31:26 +0200

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-const nextConfig = {
+module.exports = {
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-};
-
-module.exports = nextConfig;
+}
