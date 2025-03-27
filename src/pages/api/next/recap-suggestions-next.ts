0d65b407ab8fee93fc4371ed3e88ed3fee2afd12
kmnviz <kamenovivanzdravkov@gmail.com>
2025-03-27 21:46:44 +0200

    next milestone

--- src/pages/api/next/recap-suggestions-next.ts
+++ src/pages/api/next/recap-suggestions-next.ts
@@ -1,25 +1,13 @@
 // @ts-nocheck
 import type {NextApiRequest, NextApiResponse} from 'next';
-import OpenAI from 'openai';
 import SportmonksApiClient from '../../../services/sportmonksApiClient';
-import sportmonksTypes from '../../../database/sportmonks/types.json';
 import GoogleCloudStorageClient from '../../../services/googleCloudStorageClient';
 import DeepSeekService from '../../../services/deepSeekService';
+import {typeNameById} from "../../../utils";
 
 const deepSeekService = new DeepSeekService();
 const googleCloudStorageClient = new GoogleCloudStorageClient();
 const sportmonksApiClient = new SportmonksApiClient();
-const deepSeek = new OpenAI({
-  baseURL: process.env.DEEPSEEK_API_URL,
-  apiKey: process.env.DEEPSEEK_API_KEY as string,
-});
-const models = {
-  gpt4Turbo: 'gpt-4-turbo',
-  gpt4o: 'gpt-4o',
-  gpt4oLatest: 'chatgpt-4o-latest',
-  deepSeekReasoner: 'deepseek-reasoner',
-  deepSeekChat: 'deepseek-chat',
-};
 
 const fetchFixtureOutcome = async (fixtureId) => {
   const outcomeA = await sportmonksApiClient
@@ -30,70 +18,71 @@ const fetchFixtureOutcome = async (fixtureId) => {
   return { ...outcomeA, ...outcomeB };
 };
 
-const formatFixtureOutcome = (fixtureOutcome) => {
+const formatScores = (scores) => {
+  const result = {
+    total: { home: 0, away: 0 },
+    first_half: { home: 0, away: 0 },
+    second_half: { home: 0, away: 0 },
+    extra_time: { home: 0, away: 0 },
+    penalties: { home: 0, away: 0 }
+  };
 
+  scores.forEach(({ description, score }) => {
+    const participant = score.participant;
+    const goals = score.goals;
+
+    if (description === "CURRENT") {
+      result.total[participant] += goals;
+    } else if (description === "1ST_HALF") {
+      result.first_half[participant] += goals;
+    } else if (description === "2ND_HALF") {
+      result.second_half[participant] += goals;
+    } else if (description === "EXTRA_TIME") {
+      result.extra_time[participant] += goals;
+    } else if (description === "PENALTIES") {
+      result.penalties[participant] += goals;
+    }
+  });
+
+  return result;
 };
 
-// const modifyStatistics = (statistics) => {
-//   const types = sportmonksTypes;
-//
-//   const stats = statistics
-//     .map((stat) => {
-//       const type = types.find((t) => t.id === stat.type_id)
-//
-//       return {
-//         type: type.name,
-//         data: stat.data,
-//         location: stat.location,
-//       };
-//     })
-//     .flat();
-//
-//   const optimized = {};
-//
-//   stats.forEach(({type, data, location}) => {
-//     const key = type.toLowerCase().replace(/[^a-z0-9]/gi, '_'); // Normalize key names
-//
-//     if (!optimized[key]) {
-//       optimized[key] = {total: 0, home: 0, away: 0};
-//     }
-//
-//     optimized[key][location] = data.value;
-//     optimized[key].total += data.value;
-//   });
-//
-//   return optimized;
-// }
-//
-// const modifyScores = (scores) => {
-//   let result = {
-//     total: {home: 0, away: 0},
-//     first_half: {home: 0, away: 0},
-//     second_half: {home: 0, away: 0},
-//     extra_time: {home: 0, away: 0},
-//     penalties: {home: 0, away: 0}
-//   };
-//
-//   scores.forEach(({score, description}) => {
-//     if (description === "CURRENT") {
-//       result.total[score.participant] = score.goals;
-//     } else if (description === "1ST_HALF") {
-//       result.first_half[score.participant] = score.goals;
-//     } else if (description === "2ND_HALF" || description === "2ND_HALF_ONLY") {
-//       result.second_half[score.participant] = score.goals;
-//     } else if (
-//       description === "ET" ||
-//       description === "ET_1ST_HALF" ||
-//       description === "ET_2ND_HALF"
-//     ) {
-//       result.extra_time[score.participant] += score.goals;
-//     } else if (description === "PENALTY_SHOOTOUT") {
-//       result.penalties[score.participant] = score.goals;
-//     }
-//   });
-//
-//   return result;
-// }
+const formatFixtureOutcome = (fixtureOutcome) => {
+  return {
+    id: fixtureOutcome.id,
+    name: fixtureOutcome.name,
+    starting_at: fixtureOutcome.starting_at,
+    participants: fixtureOutcome.participants.map((p) => {
+      return {
+        name: p.name,
+        location: p.meta.location,
+        statistics: fixtureOutcome.statistics.filter((s) => {
+          return p.id === s.participant_id;
+        }).map((s) => {
+          return {
+            [typeNameById(s.type_id)]: s.data.value,
+          };
+        }),
+        events: fixtureOutcome.events.filter((e) => {
+          return p.id === e.participant_id;
+        }).map((e) => {
+          const result = {
+            type: typeNameById(e.type_id),
+            player_name: e.player_name,
+          };
+
+          if (e.related_player_name) result['related_player_name'] = e.related_player_name;
+          if (e.info) result['info'] = e.info;
+          if (e.addition) result['addition'] = e.addition;
+          if (e.injured) result['injured'] = e.injured;
+
+          return result;
+        }),
+      };
+    }),
+    scores: formatScores(fixtureOutcome.scores),
+  };
+};
 
 const SUGGESTIONS_DIRECTORY = 'suggestions/next';
 const RECAPS_DIRECTORY = 'recaps/next';
@@ -122,33 +111,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
 
       const outcomes = [];
-      for (let i = 0; i < (suggestions as object[]).length; i++) {
+      for (let i = 0; i < 1; i++) {
         const suggestion = (suggestions as object)[i];
-        const suggestionCompletion = suggestion.completion.data;
-        const fixtureOutcome = await fetchFixtureOutcome(suggestion.fixture.id);
-
-        return res.status(200).json({
-          fixtureOutcome: fixtureOutcome,
-        });
-
-        if (!fixtureOutcome.result_info) {
+        if (suggestion?.scores && suggestion?.scores?.length > 0) {
           console.log(`${i}:${suggestion.fixture.id} ${suggestion.fixture.name} is not finished yet`);
           continue;
         }
+
+        const suggestionCompletion = suggestion.completion.data;
+        const fixtureOutcome = formatFixtureOutcome(await fetchFixtureOutcome(suggestion.fixture.id));
 
         const suggestionCheckCompletion = await deepSeekService.createSuggestionCheckCompletion({
           suggestion: suggestionCompletion,
           outcome: fixtureOutcome,
         });
 
-        console.log('suggestionCheckCompletion: ', suggestionCheckCompletion);
-
         const outcome = {
-          fixture_id: suggestion.data.fixture.id,
-          fixture: suggestion.fixture,
+          fixture_id: suggestion.fixture.id,
+          fixture: suggestion.fixture.name,
           plan: suggestion.plan,
-          suggestion_completion: suggestionCompletion,
-          fixture_outcome: fixtureOutcome,
+          completion: suggestionCompletion,
+          outcome: fixtureOutcome,
           is_guessed: suggestionCheckCompletion['data'],
         };
         outcomes.push(outcome);
